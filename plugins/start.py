627d0b579b27ef0f3fa5dc33cb00e5de98729ed0
Nivedkc1001 <119601538+Nivedkc1001@users.noreply.github.com>
2023-05-05 23:01:20 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -59,8 +59,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filename to rename it__
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Group 🇮🇳" ,url="https://t.me/+SgufVcGFQsBjYmNl") ], 
+	[InlineKeyboardButton("Subscribe 🧐", url="https://t.me/+yWXWqxdtftM1NGM9") ]  ]))
 	    return
 	if id:
 	        if id == procode:
